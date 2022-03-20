c7c8aa83503cd7379ef379877ad1a1639adacd9b
Hok Vicheka <hvicheka.it@gmail.com>
2022-03-20 07:19:43 +0700

    Generate Book Model and Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
+
+Route::resource('books', BookController::class);
